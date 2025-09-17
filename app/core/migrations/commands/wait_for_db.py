3431635fc4899cd4e1e7bcae6bfd4bec9f80f0d2
Tibelson <elvistiburu17@gmail.com>
2025-09-17 14:48:14 +0000

    configured wait for db

--- app/core/migrations/commands/wait_for_db.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand
-
-class Command(BaseCommand):
-    """Django command to wait for database."""
-
-    def handle(self, *args, **options):
-        self.stdout.write('Waiting for database...')
