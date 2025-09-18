9de2b8ed213b3917c413d2360b0eee0c3f4ed8bd
Tibelson <elvistiburu17@gmail.com>
2025-09-18 01:27:25 +0000

    created a superuser

--- app/core/models.py
+++ app/core/models.py
@@ -14,13 +14,22 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
-    name=models.CharField(max_length=255, unique=True)
+    name=models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
